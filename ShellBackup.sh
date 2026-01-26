5c2d8c2dcb945d21864a5f74aa7822cda4aeda09
GalaXYGong <galaxygong@outlook.com>
2026-01-25 16:19:52 -0800

    simplify the Dir to be filled in Tasks

--- ShellBackup.sh
+++ ShellBackup.sh
@@ -3,6 +3,9 @@ CONFIG_FILE="$(dirname "$0")"/ShellBackup.conf
 TASKS="$(dirname "$0")"/Tasks.conf
 DATE=$(date +%Y-%m-%d)
 NOW=$(date +%Y%m%d_%H%M)
+CONTENT_FOLDER="Content"
+EXCLUDE_FILE="$(dirname "$0")"/exclude_list.txt
+EXCLUDE_OPT=""
 
 if [ -f $CONFIG_FILE ]; then
     source $CONFIG_FILE
@@ -22,39 +25,51 @@ else
     fi
 fi
 
+# when there is no exclude_list.txt, we give it empty thing
+if [ -f "$EXCLUDE_FILE" ]; then
+    EXCLUDE_OPT="--exclude-from=$EXCLUDE_FILE"
+fi
+
 if [ -f $TASKS ]; then
-    while IFS="|" read -r NAME PARENT_DIR TARGET_DIR SOURCE_DIR; do
+    while IFS="|" read -r NAME PARENT_DIR SOURCE_DIR; do
         if [[ $NAME == *\#*  ]]; then
             echo "Skipping comment: $NAME"
             continue
         fi
-        if [[ -z "$PARENT_DIR" || -z "$TARGET_DIR" ]]; then
+        if [[ -z "$PARENT_DIR" || -z "SOURCE_DIR" ]]; then
             echo "Skip Non-Tasks: $NAME"
             continue
         fi
         echo "Handleing $NAME..."
         # mkdir -p $TARGET_DIR
-        SOURCE_DIR=$(eval echo $SOURCE_DIR)
-        TARGET_DIR=$(eval echo $TARGET_DIR)
+        NAME=$(eval echo $NAME)
         PARENT_DIR=$(eval echo $PARENT_DIR)
-        TARGET_DIR="$PARENT_DIR/$TARGET_DIR"
-        BACKUP_PATH="$PARENT_DIR/changed/$DATE"
-        echo $PARENT_DIR
-        echo $SOURCE_DIR
-        echo $TARGET_DIR
-        echo $BACKUP_PATH
-        rsync -av --delete \
+        PROJECT_DIR="$PARENT_DIR/$NAME"
+        TARGET_DIR="$PROJECT_DIR/$CONTENT_FOLDER"
+        SOURCE_DIR=$(eval echo $SOURCE_DIR)
+        BACKUP_PATH="$PROJECT_DIR/Changed/$DATE"
+        echo "------------------------------------------"
+        echo -e "This is TASK $NAME.\n  Backup Content Directory \`$CONTENT_FOLDER\` will be under Parent Directory:\n    \`$TARGET_DIR\`.\n  Changed Files and Folders will be under:\n    \`$TARGET_DIR/Changed/$DATE\`"
+        echo "------------------------------------------"
+        # echo $PARENT_DIR
+        # echo $SOURCE_DIR
+        # echo $TARGET_DIR
+        # echo $BACKUP_PATH
+        mkdir -p $TARGET_DIR
+        time -p \
+            rsync -ah --delete \
               --backup \
               --backup-dir="$BACKUP_PATH" \
               --suffix="_$NOW" \
-              --exclude '.git' \
+              $EXCLUDE_OPT \
+              --stats \
+              --no-links \
               "$SOURCE_DIR" "$TARGET_DIR"
     done < $TASKS
 else
     echo "Error: Can't find Tasks file $TASKS"
     exit 1
 fi
-
 
 
 
